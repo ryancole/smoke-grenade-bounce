18c3e5eac80c55345c78400b03637d22987172ea
Ryan Cole <ryan@rycole.com>
2020-01-27 23:37:36 -0600

    first commit

---
+++ Assets/Scripts/PushNade.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class PushNade : MonoBehaviour
+{
+    public Vector3 Torque;
+    public Rigidbody Rigidbody;
+    public Transform SpawnPoint;
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Return))
+        {
+            ResetLocation();
+            SendFlying();
+
+            // enable gravity
+            Rigidbody.useGravity = true;
+        }
+    }
+
+    private void ResetLocation()
+    {
+        // place it back at spawn point
+        transform.SetPositionAndRotation(SpawnPoint.position, SpawnPoint.rotation);
+
+        // reset velocity
+        Rigidbody.velocity = new Vector3(0, 0, 0);
+        Rigidbody.angularVelocity = new Vector3(0, 0, 0);
+    }
+
+    private void SendFlying()
+    {
+        Rigidbody.AddForce(0, 0, 20, ForceMode.Impulse);
+        Rigidbody.AddTorque(Torque, ForceMode.Impulse);
+    }
+}
