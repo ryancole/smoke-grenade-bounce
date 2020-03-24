14d13c0a60def01cde4d40426fe34939aa7625c7
Ryan Cole <ryan@rycole.com>
2020-03-23 23:36:27 -0500

    add crate spin scene

---
+++ Assets/Scripts/PushCrate.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PushCrate : MonoBehaviour
+{
+    public Transform Spawn;
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Return))
+        {
+            ResetPosition();
+        }
+
+        MoveCrate();
+    }
+
+    private void MoveCrate()
+    {
+        transform.position += new Vector3(10 * Time.deltaTime, 0, 0);
+        transform.Rotate(0, 200 * Time.deltaTime, 0, Space.World);
+    }
+
+    private void ResetPosition()
+    {
+        transform.SetPositionAndRotation(Spawn.position, Spawn.rotation);
+    }
+}
